f7ef4228d3d75b5fab7df321a65a6bda3d78a60f
ASUS <gioado7>
2021-07-31 23:26:23 +0400

    final version?

--- project/src/main/java/com/example/project/Listeners/Attributes.java
+++
@@ -1,8 +0,0 @@
-package com.example.project.Listeners;
-
-
-public interface Attributes {
-    String BASE_CONNECTOR_ATTRIBUTE = "BASE";
-    String USER_MANAGER_ATTRIBUTE = "USER_MANAGER";
-    String USER_NAME_ATTRIBUTE = "user_name";
-}
---
+++ src/test/java/DataBaseTester.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+
+
+
+public class DataBaseTester {
+
+
+    @Test
+    public void test1() {
+
+    }
+}
