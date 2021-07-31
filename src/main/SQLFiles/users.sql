7c17b523036f33a7901806f07d7eefa560e58468
lukamacho <lmach19@freeuni.edu.ge>
2021-07-31 21:23:50 +0400

    FirstCommit

---
+++ src/main/SQLFiles/users.sql
@@ -0,0 +1,10 @@
+/*drop table  if exists usersTable;*/;
+
+create table usersTable(
+	id int primary key auto_increment,
+    first_name varchar(64) not null,
+    last_name varchar(64) not null,
+    user_name varchar(64) unique,
+    email varchar(64) unique,
+    password  varchar(300) not null
+);
