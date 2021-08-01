49c6fce62aeabbd170bb2ece2ed35580dd34033e
lukamacho <lmach19@freeuni.edu.ge>
2021-08-01 22:46:55 +0400

    groupsTable

---
+++ src/main/database/usersInfo.sql
@@ -0,0 +1,8 @@
+create table usersInfo(
+	user_id int not null,
+    constraint foreign key(user_id) references users(id) on delete restrict on update cascade,
+    user_name varchar(100) ,
+    user_last_name varchar(100),
+    sqesi varchar(100),
+    course varchar(100)
+);
