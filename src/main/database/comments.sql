8b6545501c8b367ab0e0a0f6579c8b1d260cb567
lukamacho <lmach19@freeuni.edu.ge>
2021-08-02 10:36:39 +0400

    groupsTable

---
+++ src/main/database/comments.sql
@@ -0,0 +1,9 @@
+create table comments(
+	comment_id int primary key auto_increment,
+    user_id int not null,
+    constraint foreign key(user_id) references users(id) on delete restrict on update cascade,
+    post_id int not null,
+    constraint foreign key(post_id) references posts(post_id) on delete restrict on update cascade,
+    comment_text varchar(3000),
+    comment_date datetime not null
+)
--- src/main/database/deleteTables.sql
+++ src/main/database/deleteTables.sql
@@ -1,3 +1,5 @@
+drop table if exists comments;
+drop table if exists posts;
 drop table if exists messages;
 drop table if exists usersInfo;
 drop table if exists groupMembers;
