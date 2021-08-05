a67e991a07b8eabea67bbffe355cb2340d429c38
George Adikashvili <adikashviligiorgi@gmail.com>
2021-08-05 19:58:14 +0400

    Savele Daos continued

--- src/main/java/Manage/ManageTrade.java
+++ src/main/java/Manage/ManageTrade.java
@@ -4,6 +4,7 @@ import DataBaseConnection.BaseConnector;
 import Manage.Configurations.SaveleConfiguration;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -14,10 +15,53 @@ public class ManageTrade implements SaveleConfiguration {
         con = bc.accessConnection();
     }
 
+    // methods checks whether location contains or not
+    public boolean isLocation(String location) throws SQLException { // to fix
+        Statement stmt = con.createStatement();
+        ResultSet rs = stmt.executeQuery("select * from " + LOCATIONS_TABLE + ";");
+
+        while (rs.next()) {
+            if (rs.getString("name").equals(location)) return true;
+        }
+        return false;
+    }
+
     public void addLocation(String location) throws SQLException { // to fix
         Statement stmt = con.createStatement();
-        stmt.execute("insert into " + LOCATIONS_TABLE + "(name, numStudents) values ('"
-                + location + "', " + 3 + ");");
+        ResultSet rs = stmt.executeQuery("select * from " + LOCATIONS_TABLE + ";");
 
+        int count = 0;
+        while (rs.next()) {
+            if (rs.getString("name").equals(location)) {
+                count = Integer.parseInt(rs.getString("numStudents"));
+            }
+        }
+        count++;
+        stmt.execute("insert into " + LOCATIONS_TABLE + "(name, numStudents) values ('"
+                + location + "', '" + count + "');");
+    }
+
+
+    public void removeLocation(String location) throws SQLException { // to fix
+        Statement stmt = con.createStatement();
+
+        boolean exists = isLocation(location); // check whether location exists or not
+        if (exists)
+            stmt.execute("delete from " + LOCATIONS_TABLE + " where name = '" + location + "';");
+        else throw new Error("location with given name isn't found");
+    }
+
+    // to fix
+    public void addStudentToLocation(String student, String location) throws SQLException {
+        Statement stmt = con.createStatement();
+        stmt.execute("insert into " + MEMBERS_TABLE + " (location_id, user_id) values ('" +
+               location + "' , ''" + student + "');'");
+    }
+
+    // to fix
+    public void removeStudentFromLocation(String student, String location) throws SQLException {
+        Statement stmt = con.createStatement();
+        stmt.execute("insert into " + MEMBERS_TABLE + " (location_id, user_id) values ('" +
+                location + "' , ''" + student + "');'");
     }
 }
