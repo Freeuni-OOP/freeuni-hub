97e872e06cf40d9628bd63c8bf31bf1b4b82841b
George Adikashvili <adikashviligiorgi@gmail.com>
2021-08-06 21:24:02 +0400

    savele pages added, small decorative fixes on main page

--- src/main/java/Manage/Configurations/SaveleConfiguration.java
+++ src/main/java/Manage/Configurations/SaveleConfiguration.java
@@ -2,6 +2,9 @@ package Manage.Configurations;
 
 
 public interface SaveleConfiguration {
+    // tables
     String LOCATIONS_TABLE = "locations";
     String MEMBERS_TABLE = "locationMembers";
+
+
 }
