4bd7e3b7c961fe8567483bcf8a4d90cf6aa7e4f5
dkhve <dkhve19@freeuni.edu.ge>
2021-08-09 14:32:13 +0400

    isFriend method added

--- src/main/java/Manage/HelperClasses/FriendAddition.java
+++ src/main/java/Manage/HelperClasses/FriendAddition.java
@@ -2,29 +2,44 @@ package Manage.HelperClasses;
 
 import DataBaseConnection.BaseConnector;
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 public class FriendAddition {
 
     BaseConnector bc;
+    Connection connection;
+
     public FriendAddition(BaseConnector bc){
         this.bc = bc;
+        connection = bc.accessConnection();
     }
 
     public void addFriend(int requester_id, int receiver_id) throws SQLException {
-        Connection connection = bc.accessConnection();
         Statement statement = connection.createStatement();
         statement.execute("Insert into friends (requester_id,receiver_id) values ("
                 + requester_id +"," + receiver_id+");");
     }
 
     public void removeFriend(int requester_id, int receiver_id) throws SQLException {
-        Connection connection = bc.accessConnection();
         Statement statement = connection.createStatement();
         statement.execute("Delete from friends where requester_id = " + requester_id
                 +" and " + " receiver_id =" + receiver_id);
         statement.execute("Delete from friends where requester_id = " + receiver_id
                 +" and " + " receiver_id =" + requester_id);
+    }
+
+    public boolean isFriend(int id1, int id2) throws SQLException {
+        // it will be saved in database either (id1, id2) or (id2, id1)
+        return isFriendHelper(id1, id2) || isFriendHelper(id2, id1);
+    }
+
+    // determines if two ids are friends where first is requester and second is receiver
+    private boolean isFriendHelper(int requester_id, int receiver_id) throws SQLException {
+        Statement statement = connection.createStatement();
+        ResultSet rs = statement.executeQuery("Select * from friends where requester_id = " + requester_id
+                +" and " + " receiver_id =" + receiver_id);
+        return rs.next();
     }
 }
--- src/main/java/Manage/HelperClasses/FriendAdditionTest.java
+++ src/main/java/Manage/HelperClasses/FriendAdditionTest.java
@@ -10,20 +10,25 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class FriendAdditionTest {
+
     BaseConnector bc;
+    Connection connection;
+    FriendAddition friendAddition;
+
     @Before
     public void setup() throws SQLException, ClassNotFoundException {
         bc = new BaseConnector();
+        connection = bc.accessConnection();
+        friendAddition = new FriendAddition(bc);
     }
 
     @Test
     public void testFriendAddition() throws SQLException {
-        FriendAddition friendAddition = new FriendAddition(bc);
-        Connection connection = bc.accessConnection();
         Statement statement = connection.createStatement();
+
         statement.execute("Insert into users (id,first_name,last_name,user_name,password,email)" +
                 " values "+ "(1000,'luka','macho','bigenti','123','fsjsffdsdfadsse')");
         statement.execute("Insert into users (id,first_name,last_name,user_name,password,email)" +
@@ -50,5 +55,48 @@ public class FriendAdditionTest {
         statement.execute("delete from users where user_name = 'bigenti';");
         statement.execute("delete from usersInfo where user_id = 2000;");
         statement.execute("delete from users where user_name = 'bigentia'");
+    }
+
+    @Test
+    public void testIsFriend() throws SQLException {
+        Statement statement = connection.createStatement();
+
+        statement.execute("Insert into users (id, first_name, last_name, user_name, password, email)" +
+                " values "+ "(108, 'qpqpqpqp', 'qpqpqpqp', 'awsawwwww', 'val1D', 'ppqpq41@freeuni.edu.ge')");
+        statement.execute("Insert into users (id, first_name, last_name, user_name, password, email)" +
+                " values "+ "(109, 'apqpqpqp', 'qpqpqpqp', 'swawwwww', 'val1D', 'apqpq42@freeuni.edu.ge')");
+        statement.execute("Insert into users (id, first_name, last_name, user_name, password, email)" +
+                " values "+ "(110, 'bpqpqpqp', 'qpqpqpqp', 'zwawwwww', 'val1D', 'bpqpq43@freeuni.edu.ge')");
+
+        statement.execute("Insert into usersInfo (user_id, user_name, user_last_name)" +
+                " values "+ "(108, 'qpqpqpqp', 'qpqpqpqp')");
+        statement.execute("Insert into usersInfo (user_id, user_name, user_last_name)" +
+                " values "+ "(109, 'apqpqpqp', 'qpqpqpqp')");
+        statement.execute("Insert into usersInfo (user_id, user_name, user_last_name)" +
+                " values "+ "(110, 'bpqpqpqp', 'qpqpqpqp')");
+
+        friendAddition.addFriend(108, 109);
+        friendAddition.addFriend(110, 108);
+
+        assertTrue(friendAddition.isFriend(108, 109));
+        assertTrue(friendAddition.isFriend(108, 110));
+        assertTrue(friendAddition.isFriend(109, 108));
+        assertTrue(friendAddition.isFriend(110, 108));
+
+        assertFalse(friendAddition.isFriend(109, 110));
+        assertFalse(friendAddition.isFriend(110, 109));
+        assertFalse(friendAddition.isFriend(108, 108));
+        assertFalse(friendAddition.isFriend(109, 109));
+        assertFalse(friendAddition.isFriend(1010, 110));
+
+        friendAddition.removeFriend(108, 109);
+        friendAddition.removeFriend(110, 108);
+
+        statement.execute("delete from usersInfo where user_id = 108;");
+        statement.execute("delete from users where id = 108;");
+        statement.execute("delete from usersInfo where user_id = 109;");
+        statement.execute("delete from users where id = 109");
+        statement.execute("delete from usersInfo where user_id = 110;");
+        statement.execute("delete from users where id = 110");
     }
 }
