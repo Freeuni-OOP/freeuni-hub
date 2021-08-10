0d10594145bb10e86215d0fa401c62e95b405aff
lukamacho <lmach19@freeuni.edu.ge>
2021-08-10 12:00:56 +0400

    commentFunctionalAdded

--- src/main/java/Manage/HelperClasses/CommentAdditionTest.java
+++ src/main/java/Manage/HelperClasses/CommentAdditionTest.java
@@ -9,32 +9,40 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-
 import static org.junit.Assert.assertEquals;
 
-public class PostAdditionTest {
-    BaseConnector bc;
+public class CommentAdditionTest {
 
+    BaseConnector bc;
     @Before
     public void setup() throws SQLException, ClassNotFoundException {
         bc = new BaseConnector();
     }
-
     @Test
-    public void testPostAddition() throws SQLException {
+    public void testCommentAddition() throws SQLException {
+        CommentAddition commentAddition = new CommentAddition(bc);
         PostAddition postAddition = new PostAddition(bc);
         Connection connection = bc.accessConnection();
         Statement statement =connection.createStatement();
+        commentAddition.removeComment(1000,100);
         postAddition.removePost(2000);
         statement.execute("delete from users where id = 1000;");
         statement.execute("Insert into users (id,first_name,last_name,user_name,password,email)" +
                 " values "+ "(1000,'luka','macho','mlfakfflsalme','123','MACSE')");
         postAddition.addPost(1000,"This is my first post",2000);
+        commentAddition.addComment(1000,2000,100,"My first comment.");
+        ResultSet rs = statement.executeQuery("Select * from comments");
+        while(rs.next()){
+            assertEquals(100,rs.getInt(1));
+            assertEquals(2000,rs.getInt(3));
+            assertEquals(1000,rs.getInt(2));
+        }
         ResultSet resultSet = statement.executeQuery("Select * from posts");
         while(resultSet.next()){
             assertEquals(1000,resultSet.getInt(2));
             assertEquals(2000,resultSet.getInt(1));
         }
+        commentAddition.removeComment(1000,100);
         postAddition.removePost(2000);
         statement.execute("delete from users where id = 1000;");
 
